1dd3d3d5e3e85a29c5e59ce2df228e1debff4e62
gabrielmj23 <gabrielmj2312@gmail.com>
2022-02-21 11:44:31 -0400

    Added Book Instance delete functionality, page and button

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -83,13 +83,29 @@ exports.bookinstance_create_post = [
 ];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = function(req, res, next) {
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function(err, bookinstance) {
+        if (err) { return next(err); }
+        // No results
+        if (bookinstance == null) {
+            var err = new Error('Book copy not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('bookinstance_delete', {title: 'Delete Book Instance', bookinstance: bookinstance});
+    })
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = function(req, res, next) {
+    BookInstance.findByIdAndRemove(req.body.bookinstanceid, function DeleteBookInstance(err) {
+        if (err) { return next(err); }
+        // Success
+        res.redirect('/catalog/bookinstances');
+    });
 };
 
 // Display BookInstance update form on GET.
